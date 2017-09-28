dce02109eafcc2aa5a3a2d2e979eed06b4ed460a
1305715931@qq.com <1305715931@qq.com>
2017-09-28 09:46:42 +0800

    test

---
+++ girl/src/main/java/com/imooc/girl/HelloController.java
@@ -0,0 +1,19 @@
+package com.imooc.girl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+@Autowired
+private GirlProperties girlProperties;
+
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String say(){
+        return girlProperties.getCupSize();
+    }
+}
